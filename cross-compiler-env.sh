a979e9024565548126616aed1df62ce217cbecef
Adam Steen <adam@adamsteen.com.au>
2017-07-26 17:58:55 +0800

    remove cross-compiler-env.sh

--- cross-compiler-env.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/sh
-
-export AS=$PWD/.cross/bin/x86_64-elf-as
-export CC=$PWD/.cross/bin/x86_64-elf-gcc
-export LD=$PWD/.cross/bin/x86_64-elf-ld
-export OBJCOPY=$PWD/.cross/bin/x86_64-elf-objcopy
